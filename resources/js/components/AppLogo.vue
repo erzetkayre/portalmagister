d9d8af85ec9b372ac3071609605bff0cfc6d3ff9
erzetkayre <erzetkayre@gmail.com>
2025-08-05 13:12:09 +0700

    pwk sementara2

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -7,7 +7,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-medium">Portal Magister Elektro</span>
+        <span class="mb-0.5 truncate leading-tight font-medium">Portal Magister PWK</span>
         <span class="mb-0.5 truncate leading-tight font-reguler text-xs">Fakultas Teknik UNS</span>
     </div>
 </template>
